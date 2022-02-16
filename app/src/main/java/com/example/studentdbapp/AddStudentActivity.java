029997399f372453f1974836b1edd8fea1d8cf0f
snehasamuel <snehasamuel297@gmail.com>
2022-02-16 10:03:14 +0530

    upto search student

--- app/src/main/java/com/example/studentdbapp/AddStudentActivity.java
+++ app/src/main/java/com/example/studentdbapp/AddStudentActivity.java
@@ -1,14 +1,56 @@
 package com.example.studentdbapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class AddStudentActivity extends AppCompatActivity {
+
+
+    EditText ed1,ed2,ed3,ed4;
+    AppCompatButton b1,b2;
+
+    String getName,getRollNumber,getAdmission,getCollege;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_student);
+        ed1=(EditText) findViewById(R.id.name);
+        ed2=(EditText) findViewById(R.id.number);
+        ed3=(EditText) findViewById(R.id.admsn);
+        ed4=(EditText) findViewById(R.id.college);
+        b1=(AppCompatButton) findViewById(R.id.submit);
+        b2=(AppCompatButton) findViewById(R.id.backto);
+
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getName=ed1.getText().toString();
+                getRollNumber=ed2.getText().toString();
+                getAdmission=ed3.getText().toString();
+                getCollege=ed4.getText().toString();
+
+                Toast.makeText(getApplicationContext(), getName, Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(),getRollNumber,Toast.LENGTH_SHORT);
+                Toast.makeText(getApplicationContext(),getAdmission , Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(),getCollege , Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(i);
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/studentdbapp/SearchStudentAcitivity.java
+++ app/src/main/java/com/example/studentdbapp/SearchStudentAcitivity.java
@@ -1,14 +1,45 @@
 package com.example.studentdbapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class SearchStudentAcitivity extends AppCompatActivity {
 
+    EditText ed1,ed2,ed3,ed4;
+    AppCompatButton b1,b2;
+
+    String getAdmission,getName,getNumber,getCollege;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_student_acitivity);
+        ed1=(EditText) findViewById(R.id.Admsn);
+        ed2=(EditText) findViewById(R.id.name);
+        ed3=(EditText) findViewById(R.id.number);
+        ed4=(EditText) findViewById(R.id.college);
+        b1=(AppCompatButton) findViewById(R.id.search);
+        b2=(AppCompatButton) findViewById(R.id.Backto);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getAdmission=ed1.getText().toString();
+                Toast.makeText(getApplicationContext(), getAdmission, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
