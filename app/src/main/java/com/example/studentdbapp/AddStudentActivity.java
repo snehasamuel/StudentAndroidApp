8f04371cd150491f91a311ee49314811d9724a52
snehasamuel <snehasamuel297@gmail.com>
2022-02-16 15:21:10 +0530

    StudentApp completed

--- app/src/main/java/com/example/studentdbapp/AddStudentActivity.java
+++ app/src/main/java/com/example/studentdbapp/AddStudentActivity.java
@@ -17,10 +17,15 @@ public class AddStudentActivity extends AppCompatActivity {
 
     String getName,getRollNumber,getAdmission,getCollege;
 
+    DatabaseHelper dbhelper;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_student);
+
+        dbhelper=new DatabaseHelper(this);
+        dbhelper.getWritableDatabase();
         ed1=(EditText) findViewById(R.id.name);
         ed2=(EditText) findViewById(R.id.number);
         ed3=(EditText) findViewById(R.id.admsn);
@@ -37,10 +42,20 @@ public class AddStudentActivity extends AppCompatActivity {
                 getAdmission=ed3.getText().toString();
                 getCollege=ed4.getText().toString();
 
-                Toast.makeText(getApplicationContext(), getName, Toast.LENGTH_SHORT).show();
-                Toast.makeText(getApplicationContext(),getRollNumber,Toast.LENGTH_SHORT);
-                Toast.makeText(getApplicationContext(),getAdmission , Toast.LENGTH_SHORT).show();
-                Toast.makeText(getApplicationContext(),getCollege , Toast.LENGTH_SHORT).show();
+              boolean result=dbhelper.insertData(getName,getRollNumber,getAdmission,getCollege);
+
+              if (result==true)
+              {
+                  ed1.setText("");
+                  ed2.setText("");
+                  ed3.setText("");
+                  ed4.setText("");
+                  Toast.makeText(getApplicationContext(), "sucessfully Inserted", Toast.LENGTH_SHORT).show();
+              }
+                  else
+              {
+                  Toast.makeText(getApplicationContext(), "Data failed to insert", Toast.LENGTH_SHORT).show();
+              }
             }
         });
 
---
+++ app/src/main/java/com/example/studentdbapp/DatabaseHelper.java
@@ -0,0 +1,65 @@
+package com.example.studentdbapp;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    static String DbName="college.db";
+    static String TableName="Student";
+    static String Col1="Id";
+    static String Col2="Name";
+    static String Col3="RollNo";
+    static String Col4="AdmissionNo";
+    static String Col5="College";
+
+
+    public DatabaseHelper(Context context) {
+        super(context, DbName, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+        String query="create table "+TableName+"(" +
+                Col1+" integer primary key autoincrement,"+
+                Col2+" text,"+
+                Col3+" text,"+
+                Col4+" text,"+
+                Col5+" text)";
+
+        db.execSQL(query);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+
+    public boolean insertData(String name,String rollno,String admsnno,String clg)
+    {
+        SQLiteDatabase db=this.getWritableDatabase();
+        ContentValues c=new ContentValues();
+        c.put(Col2,name);
+        c.put(Col3,rollno);
+        c.put(Col4,admsnno);
+        c.put(Col5,clg);
+
+        long status=db.insert(TableName,null,c);
+        if(status==-1)
+        {
+            return  false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+
+
+}
